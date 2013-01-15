266f9e46121cde81f6b57a3f81a4a18fc8dd7c88
Sergei Datsenko <dosyas@yandex.ru>
2013-01-15 12:51:05 +0400

    Try to parse the page

--- chrome/popup.js
+++ chrome/popup.js
@@ -2,82 +2,136 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'kittens';
+// This extension demonstrates using chrome.downloads.download() to
+// download URLs.
 
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
+var allLinks = [];
+var visibleLinks = [];
 
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
-
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
-
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
+// Display all visible links.
+function showLinks() {
+  var linksTable = document.getElementById('links');
+  while (linksTable.children.length > 1) {
+    linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
   }
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    var row = document.createElement('tr');
+    var col0 = document.createElement('td');
+    var col1 = document.createElement('td');
+    var checkbox = document.createElement('input');
+    checkbox.checked = true;
+    checkbox.type = 'checkbox';
+    checkbox.id = 'check' + i;
+    col0.appendChild(checkbox);
+    col1.innerText = visibleLinks[i];
+    col1.style.whiteSpace = 'nowrap';
+    col1.onclick = function() {
+      checkbox.checked = !checkbox.checked;
+    }
+    row.appendChild(col0);
+    row.appendChild(col1);
+    linksTable.appendChild(row);
+  }
+}
+
+// Toggle the checked state of all visible links.
+function toggleAll() {
+  var checked = document.getElementById('toggle_all').checked;
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    document.getElementById('check' + i).checked = checked;
+  }
+}
+
+// Download all visible checked links.
+function downloadCheckedLinks() {
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    if (document.getElementById('check' + i).checked) {
+      chrome.downloads.download({url: visibleLinks[i]},
+                                             function(id) {
+      });
+    }
+  }
+  window.close();
+}
+
+// Re-filter allLinks into visibleLinks and reshow visibleLinks.
+function filterLinks() {
+  var filterValue = document.getElementById('filter').value;
+  if (document.getElementById('regex').checked) {
+    visibleLinks = allLinks.filter(function(link) {
+      return link.match(filterValue);
+    });
+  } else {
+    var terms = filterValue.split(' ');
+    visibleLinks = allLinks.filter(function(link) {
+      for (var termI = 0; termI < terms.length; ++termI) {
+        var term = terms[termI];
+        if (term.length != 0) {
+          var expected = (term[0] != '-');
+          if (!expected) {
+            term = term.substr(1);
+            if (term.length == 0) {
+              continue;
+            }
+          }
+          var found = (-1 !== link.indexOf(term));
+          if (found != expected) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
+  }
+  showLinks();
+}
+
+// Add links to allLinks and visibleLinks, sort and show them.  send_links.js is
+// injected into all frames of the active tab, so this listener may be called
+// multiple times.
+chrome.extension.onRequest.addListener(function(links) {
+  for (var index in links) {
+    allLinks.push(links[index]);
+  }
+  allLinks.sort();
+  visibleLinks = allLinks;
+  showLinks();
+});
+
+// Set up event handlers and inject send_links.js into all frames in the active
+// tab.
+window.onload = function() {
+  document.getElementById('filter').onkeyup = filterLinks;
+  document.getElementById('regex').onchange = filterLinks;
+  document.getElementById('toggle_all').onchange = toggleAll;
+  document.getElementById('download0').onclick = downloadCheckedLinks;
+  document.getElementById('download1').onclick = downloadCheckedLinks;
+
+  chrome.windows.getCurrent(function (currentWindow) {
+    chrome.tabs.query({active: true, windowId: currentWindow.id},
+                      function(activeTabs) {
+      chrome.tabs.executeScript(
+        activeTabs[0].id, {file: 'send_links.js', allFrames: true});
+    });
+  });
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
+
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+function click(e) {
+  chrome.tabs.executeScript(null,
+      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+  window.close();
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
 });
+*/
